13ab5f1e414d9d2fcf89de9ea68c8b0781712893
Matheus <ma.delara@duocuc.cl>
2026-03-26 15:12:49 -0300

    🚀🚀🚀🚀 v1.0.0 PRIMER DESPLIEGUE OFICIAL KUHUB EN AWS

--- backend/src/main/java/KuHub/config/security/SpringSecurityConfig.java
+++ backend/src/main/java/KuHub/config/security/SpringSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
