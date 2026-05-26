4f35d4e2bb0d7be449364387e727c94d189fe7ae
Matheus <profession25cl.br@gmail.com>
2026-05-26 12:49:56 -0400

    Modificaciones en proveedores- ORDEN PEDIDO--

---
+++ frontend/dist/assets/usePageTitle-BOMGy260.js
@@ -0,0 +1,1 @@
+import{a as o}from"./react-vendor-BxMPNXTO.js";import{x as r}from"./index-Bqm2HIV9.js";const p=(e,t="",s="")=>{const{setPageTitle:a}=r();o.useEffect(()=>{a(e,t,s)},[e,t,s,a])};export{p as u};
